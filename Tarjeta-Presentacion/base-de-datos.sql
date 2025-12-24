421841c730008697426ed96bf5c0ee3b89d0935a
Juan Soto <cansino.juan.jose@gmail.com>
2025-12-24 09:07:53 -0600

    Cambio de color fondo

--- Tarjeta-Presentacion/base-de-datos.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE ProductsDB;
-USE ProductsDB;
-CREATE TABLE Products (
-    ProductID INT PRIMARY KEY AUTO_INCREMENT,
-    ProductName VARCHAR(100) NOT NULL,
-    Price DECIMAL(10, 2) NOT NULL,
-    Stock INT NOT NULL
-);
